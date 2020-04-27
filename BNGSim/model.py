90fa45f7da73587046664dedf11fe57a1344d308
Ali Sinan Saglam <asinansaglam@gmail.com>
2020-04-27 09:56:32 -0400

    BNGL2.pl output re-enabled when loading a model

--- BNGSim/model.py
+++ BNGSim/model.py
@@ -73,7 +73,7 @@ class BNGModel:
         # run with --xml 
         os.chdir(temp_folder)
         # TODO: Make output supression an option somewhere
-        rc = subprocess.run([self.bngexec, "--xml", stripped_bngl], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        rc = subprocess.run([self.bngexec, "--xml", stripped_bngl])
         if rc.returncode == 1:
             print("XML generation failed, trying the fallback parser")
             # go back to our original location
