67ad515a84f2891e6963860bc76deb90b189c483
Ali Sinan Saglam <asinansaglam@gmail.com>
2020-05-13 14:44:23 -0400

    Ticked up version number because of the simulation features

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ meta = pkg_utils.get_package_metadata(dirname, name)
 
 setuptools.setup(
         name="BNGSim",
-        version="0.1.1",
+        version="0.2.0",
         author="Ali Sinan Saglam",
         author_email="asinansaglam@gmail.com",
         description="A simple python front-end for BioNetGen simulations",
