bd7e57e9471c9f4946ee0e5566528cf1e089b296
Ali Sinan Saglam <asinansaglam@gmail.com>
2020-05-28 15:43:12 -0400

    Major revamp on how patterns work. Everything is an object, attributes are now properties so that there is an extra layer on top of each to implement internal logic. This prepares for user-facing methods that includes internal logic while keeping an underlying structure for developer use

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ meta = pkg_utils.get_package_metadata(dirname, name)
 
 setuptools.setup(
         name="BNGSim",
-        version="0.3.21",
+        version="0.3.23",
         author="Ali Sinan Saglam",
         author_email="asinansaglam@gmail.com",
         description="A simple python front-end for BioNetGen simulations",
